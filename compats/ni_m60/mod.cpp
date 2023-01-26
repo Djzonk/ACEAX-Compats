3db4d6826bef9df4c011131430cdf014202f88a1
Djzonk <dannyjupp24@gmail.com>
2023-01-25 20:17:02 -0600

    [NIM60] Init

---
+++ compats/ni_m60/mod.cpp
@@ -0,0 +1,6 @@
+name = "ACEAX NIArms M60 Compat";
+picture = "logo_nim60_ca.paa";
+actionName = "GitHub";
+action = "https://github.com/Djzonk/ACEAX-Compats";
+logo = "logo_nim60_ca.paa";
+logoOver = "logo_nim60_ca.paa";
