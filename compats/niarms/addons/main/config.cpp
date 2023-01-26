978ffde0e08eb9b143b1e5af861c3cada063139e
DJZonk <dannyjupp24@gmail.com>
2023-01-26 04:40:12 -0600

    [NIArms] Branch Init

---
+++ compats/niarms/addons/main/config.cpp
@@ -0,0 +1,24 @@
+#include "mod_macros.hpp"
+class CfgPatches {
+    class ADDON {
+        name = QUOTE(COMPONENT);
+        units[] = {};
+        weapons[] = {};
+        requiredVersion = REQUIRED_VERSION;
+        requiredAddons[] = {"cba_main", "aceax_main", "hlcweapons_core"};
+        author = "DJZonk";
+        VERSION_CONFIG;
+    };
+};
+
+class XtdGearModels {
+    class CfgWeapons {
+
+    };
+};
+
+class XtdGearInfos {
+    class CfgWeapons {
+
+    };
+};
---
+++ compats/niarms/mod.cpp
@@ -0,0 +1,6 @@
+name = "ACEAX NIArms Compat";
+picture = "logo_niarms_co.paa";
+actionName = "GitHub";
+action = "https://github.com/Djzonk/ACEAX-Compats";
+logo = "logo_niarms_co.paa";
+logoOver = "logo_niarms_co.paa";
