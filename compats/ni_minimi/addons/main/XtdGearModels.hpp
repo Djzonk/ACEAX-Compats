a74128a84395270f03dca0701cf576bd622afa2a
Djzonk <dannyjupp24@gmail.com>
2023-01-25 18:18:45 -0600

    [NIMinimi] work

--- compats/ni_minimi/addons/main/XtdGearModels.hpp
+++ compats/ni_minimi/addons/main/XtdGearModels.hpp
@@ -1,5 +1,117 @@
 class XtdGearModels {
     class CfgWeapons {
-
+        class GVAR(mk46) {
+            label = "MK46";
+            author = "Toadie | ACEAX Compat By DJZonk";
+            options[] = {"mod"};
+            class mod {
+                label = "Mod";
+                values[] = {"mod0", "mod1"};
+                class mod0 {
+                    label = "Mod 0";
+                };
+                class mod1 {
+                    label = "Mod 1";
+                };
+            };
+        };
+        class GVAR(mk48): GVAR(mk46) {label = "MK48";};
+        class GVAR(m249) {
+            label = "M249";
+            author = "Toadie | ACEAX Compat By DJZonk";
+            options[] = {"varient","barrel", "stock", "rail"};
+            class varient {
+                label = "Varient";
+                alwaysSelectable = 1;
+                values[] = {"base", "para", "pip"};
+                class base {
+                    label = "Base";
+                };
+                class para {
+                    label = "Para";
+                };
+                class pip {
+                    label = "PIP";
+                    description = "Product Improvement Program kit";
+                };
+            };
+            class barrel {
+                label = "Barrel Length";
+                alwaysSelectable = 1;
+                values[] = {"short", "long"};
+                class short {
+                    label = "Short";
+                };
+                class long {
+                    label = "Long";
+                };
+            };
+            class stock {
+                label = "Stock";
+                alwaysSelectable = 1;
+                values[] = {"solid", "para", "pip", "frame"};
+                class solid {
+                    label = "Solid";
+                };
+                class para {
+                    label = "Para";
+                };
+                class pip {
+                    label = "PIP";
+                };
+                class frame {
+                    label = "Frame";
+                };
+            };
+            class rail {
+                label = "Optics Rail";
+                alwaysSelectable = 1;
+                values[] = {"no", "yes"};
+                class no {
+                    label = "No";
+                };
+                class yes {
+                    label = "Yes";
+                };
+            };
+        };
+        class GVAR(minimi) {
+            label = "FN Minimi";
+            author = "Toadie | ACEAX Compat By DJZonk";
+            options[] = {"stock", "barrel", "rail"};
+            class stock {
+                label = "Stock";
+                alwaysSelectable = 1;
+                values[] = {"solid", "para"};
+                class solid {
+                    label = "Solid";
+                };
+                class para {
+                    label = "Para";
+                };
+            };
+            class barrel {
+                label = "Barrel Length";
+                alwaysSelectable = 1;
+                values[] = {"short", "long"};
+                class short {
+                    label = "Short";
+                };
+                class long {
+                    label = "Long";
+                };
+            };
+            class rail {
+                label = "Optic Rail";
+                alwaysSelectable = 1;
+                values[] = {"no", "yes"};
+                class no {
+                    label = "No";
+                };
+                class yes {
+                    label = "Yes";
+                };
+            };
+        };
     };
 };
