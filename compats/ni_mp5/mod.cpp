11b205e085645cb4679ec6bb32bcf50fc8b03c79
Djzonk <dannyjupp24@gmail.com>
2023-01-25 18:25:14 -0600

    [NIMP5] Init

---
+++ compats/ni_mp5/mod.cpp
@@ -0,0 +1,6 @@
+name = "ACEAX NIArms MP5 Compat";
+picture = "logo_nimp5_ca.paa";
+actionName = "GitHub";
+action = "https://github.com/Djzonk/ACEAX-Compats";
+logo = "logo_nimp5_ca.paa";
+logoOver = "logo_nimp5_ca.paa";
