f5d0ed144e33664e7c4576759a4f1efece2a22cb
Djzonk <dannyjupp24@gmail.com>
2023-01-31 00:24:32 -0600

    [Repo] remove aio template

--- extra/template_aio/mod.cpp
+++
@@ -1,6 +0,0 @@
-name = "TMPL_mod_name";
-picture = "logo_compat_ca.paa";
-actionName = "GitHub";
-action = "https://github.com/Djzonk/ACEAX-Compats";
-logo = "logo_compat_ca.paa";
-logoOver = "logo_compat_ca.paa";
